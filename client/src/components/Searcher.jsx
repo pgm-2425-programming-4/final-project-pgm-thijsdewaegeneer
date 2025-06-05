2b5fec95c62e8de36a90bfd7d6daad40d06a2f49
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-05 16:40:59 +0200

    Changes in layout

--- client/src/components/Searcher.jsx
+++ client/src/components/Searcher.jsx
@@ -3,12 +3,16 @@
         return (
             <section className="searcher">
 
+                <form action="submit" method="post" className="searcher__input">
+                    <input type="text" name="query" placeholder="Search by description"></input>
+                </form>
+
                 <h2 className="searcher__active-project">Active project: PGM3</h2>
 
                 <div className="searcher__buttons">
 
                     <button className="searcher__button searcher__button--left">Add new task</button>
-                    <button className="searcher__button searcher__button--right"><a href="/backlog" className="searcher__button link">View backlog</a></button>
+                    <button className="searcher__button searcher__button--right"><a href="/backlog" className="searcher__button-link">View backlog</a></button>
 
                 </div>
             </section>
