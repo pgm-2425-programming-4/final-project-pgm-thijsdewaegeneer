96a879248bc4813585bf2b8408bd8a293ad774c7
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-05 11:19:14 +0200

    Added Searcher component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,18 @@
-import Navigation from "./Navigation"
-import "../styles/base/main.css"
+import Navigation from "./Navigation";
+import Searcher from "./Searcher"
+import "../styles/base/main.css";
 
 function App() {
     console.log("App.jsx succesfully loaded in")
     return(
+        <>
         <header>
             <Navigation /> 
         </header>
+        <main>
+            <Searcher />
+        </main>
+        </>
     )
 }
 export default App
