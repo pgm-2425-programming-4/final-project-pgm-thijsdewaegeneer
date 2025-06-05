0c33674a086821d05108d4a2125259cf37d60d5e
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-05 11:46:33 +0200

    Populated Searcher with elements

--- client/src/components/Searcher.jsx
+++ client/src/components/Searcher.jsx
@@ -1,5 +1,18 @@
     function Searcher () {
         console.log("Searcher loaded in succesfully")
+        return (
+            <section className="searcher">
+
+                <h2 className="searcher__active-project">Active project: PGM3</h2>
+
+                <div className="searcher__buttons">
+
+                    <button className="searcher__button searcher__button--left">Add new task</button>
+                    <button className="searcher__button searcher__button--right"><a href="/backlog" className="searcher__button link">View backlog</a></button>
+
+                </div>
+            </section>
+        )
         
     }
 
