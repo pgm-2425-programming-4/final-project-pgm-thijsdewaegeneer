c35fa70a77e784e7f3a8a4a347be1bfc40bfa951
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-09 21:12:57 +0200

    Added columns

--- client/src/components/Column.jsx
+++ client/src/components/Column.jsx
@@ -1,6 +1,59 @@
 function Column () {
     console.log("Column succesfully loaded in")
-    return
+    return (
+        <>
+
+        <div className="board__list-wrapper">
+            <h2 className="board__list-title">To Do</h2>
+            <ul className="board__list">
+                <li className="board__list-item">
+                <div className="card">
+                    <h3 className="card__title"></h3>
+                    <p className="card__context">
+                    </p>
+                </div>
+                </li>
+            </ul>
+        </div>
+
+        <div className="board__list-wrapper">
+        <h2 className="board__list-title">In Progress</h2>
+        <ul className="board__list">
+            <li className="board__list-item">
+            <div className="card">
+                <h3 className="card__title"></h3>
+                <p className="card__context"></p>
+            </div>
+            </li>
+        </ul>
+        </div>
+
+        <div className="board__list-wrapper">
+        <h2 className="board__list-title">Ready for Review</h2>
+        <ul className="board__list">
+            <li className="board__list-item">
+            <div className="card">
+                <h3 className="card__title"></h3>
+                <p className="card__context">.</p>
+            </div>
+            </li>
+        </ul>
+        </div>
+
+        <div className="board__list-wrapper">
+        <h2 className="board__list-title">Done</h2>
+        <ul className="board__list">
+            <li className="board__list-item">
+            <div className="card">
+                <h3 className="card__title"></h3>
+                <p className="card__context"></p>
+            </div>
+            </li>
+        </ul>
+        </div>
+        </>
+        
+    )
 }
 
 export default Column
