d4c6acab84cf503739e201199db09321031ae092
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-11 18:47:34 +0200

    dynamically loaded in statuses and filtered out backlog

--- client/src/components/Column.jsx
+++ client/src/components/Column.jsx
@@ -1,57 +1,42 @@
+import {useQuery} from "@tanstack/react-query";
+import { fetchStatus } from "../api/categories/FetchCategory";
+import React from "react";
+
 function Column () {
+    const { isPending, isError, data, error } = useQuery({ 
+    queryKey: ['statuses'], 
+    queryFn: fetchStatus
+    });
+
+    if(isPending) {
+        return <span>Loading...</span>
+    }
+    if(isError) {
+        return <span>Error: {error.message}</span>
+    }
     console.log("Column succesfully loaded in")
+
     return (
-        <>
+    <>
 
-        <div className="board__list-wrapper">
-            <h2 className="board__list-title">To Do</h2>
-            <ul className="board__list">
-                <li className="board__list-item">
-                <div className="card">
-                    <h3 className="card__title"></h3>
-                    <p className="card__context">
-                    </p>
-                </div>
-                </li>
-            </ul>
-        </div>
-
-        <div className="board__list-wrapper">
-        <h2 className="board__list-title">In Progress</h2>
-        <ul className="board__list">
+      {data.data
+      .filter(status => status.StatusType !== "Backlog")
+      .map((status) => (
+        <div key={status.id} className="board__list-wrapper">
+          <h2 className="board__list-title">{status.StatusType}</h2>
+          <ul className="board__list">
             <li className="board__list-item">
-            <div className="card">
-                <h3 className="card__title"></h3>
-                <p className="card__context"></p>
-            </div>
+              <div className="card">
+                <h3 className="card__title">Dummy text</h3>
+                <p className="card__context">Dummy text</p>
+              </div>
             </li>
-        </ul>
+          </ul>
         </div>
+      ))
+      }
 
-        <div className="board__list-wrapper">
-        <h2 className="board__list-title">Ready for Review</h2>
-        <ul className="board__list">
-            <li className="board__list-item">
-            <div className="card">
-                <h3 className="card__title"></h3>
-                <p className="card__context"></p>
-            </div>
-            </li>
-        </ul>
-        </div>
-
-        <div className="board__list-wrapper">
-        <h2 className="board__list-title">Done</h2>
-        <ul className="board__list">
-            <li className="board__list-item">
-            <div className="card">
-                <h3 className="card__title"></h3>
-                <p className="card__context"></p>
-            </div>
-            </li>
-        </ul>
-        </div>
-        </>
+    </>
         
     )
 }
