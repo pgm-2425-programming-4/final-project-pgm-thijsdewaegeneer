2b52ba4c21058afb38f9cf9d75193851c8bf0e10
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-11 20:38:35 +0200

    Added logic to dynamically load in tasks

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,15 +1,40 @@
+import {useQuery} from "@tanstack/react-query";
+import { fetchTask } from "../api/tasks/FetchTask"
+
 function TaskList() {
 
+  
+    const { isPending, isError, data, error } = useQuery({ 
+    queryKey: ['tasks'], 
+    queryFn: fetchTask
+    });
+
+    if(isPending) {
+    return <span>Loading...</span>
+    }
+
+    if(isError) {
+    return <span>Error: {error.message}</span>
+    }
+    console.log("Tasklist succesfully loaded in")
+
     return (
-          <ul className="board__list">
-            <li className="board__list-item">
-              <div className="card">
-                <h3 className="card__title">Dummy text</h3>
-                <p className="card__context">Dummy text</p>
-              </div>
-            </li>
-          </ul>
+      <>
+        {data.data
+        .map((task) => (
+            <ul key={task.id} className="board__list">
+              <li className="board__list-item">
+                <div className="card">
+                  <h3 className="card__title">{task.title}</h3>
+                  <p className="card__context">{task.description}</p>
+                </div>
+              </li>
+            </ul>
+        ))
+        }
+      </>
     )
 }
+
 
 export default TaskList
