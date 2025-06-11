daa85f73d7c961ad534f4aa504f92da1334b95e0
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-11 18:56:47 +0200

    New component for actual tasklist

---
+++ client/src/components/TaskList.jsx
@@ -0,0 +1,15 @@
+function TaskList() {
+
+    return (
+          <ul className="board__list">
+            <li className="board__list-item">
+              <div className="card">
+                <h3 className="card__title">Dummy text</h3>
+                <p className="card__context">Dummy text</p>
+              </div>
+            </li>
+          </ul>
+    )
+}
+
+export default TaskList
