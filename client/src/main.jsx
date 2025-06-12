cf77758984ed67ba4d335d232bda9e21d3a5882e
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-12 10:33:05 +0200

    Removed App component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
 import App from "./components/App.jsx"
 
 const queryClient = new QueryClient();
+const router = createRouter({ routeTree })
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
 )
