9343c91c16882537fd95b5447162ee48353f5f9d
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-12 10:36:33 +0200

    Added fetch and mapping for tasks

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,40 +1,40 @@
-import {useQuery} from "@tanstack/react-query";
-import { fetchTask } from "../api/tasks/FetchTask"
+import { useQuery } from "@tanstack/react-query";
+import { fetchTask } from "../api/tasks/FetchTask";
 
-function TaskList() {
-
-  
-    const { isPending, isError, data, error } = useQuery({ 
-    queryKey: ['tasks'], 
+function TaskList({ statusName }) {
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ['tasks'],
     queryFn: fetchTask
-    });
+  });
 
-    if(isPending) {
-    return <span>Loading...</span>
-    }
+  if (isPending) {
+    return <span>Loading...</span>;
+  }
 
-    if(isError) {
-    return <span>Error: {error.message}</span>
-    }
-    console.log("Tasklist succesfully loaded in")
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
 
-    return (
-      <>
-        {data.data
-        .map((task) => (
-            <ul key={task.id} className="board__list">
-              <li className="board__list-item">
-                <div className="card">
-                  <h3 className="card__title">{task.title}</h3>
-                  <p className="card__context">{task.description}</p>
-                </div>
-              </li>
-            </ul>
-        ))
-        }
-      </>
-    )
+  // Safely filter tasks only if data and data.data exist
+  const filteredTasks = data?.data
+    ? data.data.filter(
+        task =>
+          task.attributes?.status?.data?.attributes?.name === statusName
+      )
+    : [];
+
+  return (
+<ul className="board__list">
+  {filteredTasks.map((task) => (
+    <li key={task.id} className="board__list-item">
+      <div className="card">
+        <h3 className="card__title">{task.Title}</h3>
+        <p className="card__context">{task.Description}</p>
+      </div>
+    </li>
+  ))}
+</ul>
+  );
 }
 
-
-export default TaskList
+export default TaskList;
