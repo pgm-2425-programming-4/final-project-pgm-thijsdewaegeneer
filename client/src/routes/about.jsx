553579db14479a847cca6567d23185aee1e429b1
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-14 21:20:54 +0200

    styling

--- client/src/routes/about.jsx
+++ client/src/routes/about.jsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import Navigation from '../components/Navigation'
 
 export const Route = createFileRoute('/about')({
   component: RouteComponent,
@@ -9,16 +8,23 @@ function RouteComponent() {
   return (
     <>
         <section className='about'>
+
+          <div className='about__content'>
           <h2 className='about__title'>Over dit project</h2> <br />
-          <p className='about__context'>
-          Dit project is ontwikkeld in het kader van een opdracht aan Arteveldehogeschool. Het doel is om mijn vaardigheden in webontwikkeling, ontwerp en/of contentcreatie te demonstreren door een praktische toepassing te bouwen. Tijdens het project heb ik aandacht besteed aan gebruiksvriendelijkheid, responsief design en technische kwaliteit. Het resultaat is een functionele en toegankelijke webapplicatie die inspeelt op een specifieke behoefte of doelgroep.
-          </p> <br />
-          <p>
+            <p className='about__context'>
+              Dit project is ontwikkeld in het kader van een opdracht aan Arteveldehogeschool. Het doel is om mijn vaardigheden in webontwikkeling, ontwerp en/of contentcreatie te demonstreren door een praktische toepassing te bouwen. Tijdens het project heb ik aandacht besteed aan gebruiksvriendelijkheid, responsief design en technische kwaliteit. Het resultaat is een functionele en toegankelijke webapplicatie die inspeelt op een specifieke behoefte of doelgroep.
+            </p>
+          </div>
+
+          <div className='about__content'>
             <h2 className='about__title'>Contact</h2>
-            Naam: Thijs De Waegeneer <br />
-            E-mail: [jouw.studentnummer]@student.arteveldehs.be <br />
-            LinkedIn: 
-          </p>
+            
+              <p className='about__context'>
+                Naam: Thijs De Waegeneer <br />
+                E-mail: [jouw.studentnummer]@student.arteveldehs.be <br />
+                LinkedIn: 
+              </p>
+          </div>
         </section>
     </>
 )
