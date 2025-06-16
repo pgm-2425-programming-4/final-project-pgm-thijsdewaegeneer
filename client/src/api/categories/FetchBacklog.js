93a4a2164e6280d5b797fa6b72aa52154a6161d5
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-06-17 01:06:51 +0200

    Added fetch backlog exclusively

---
+++ client/src/api/categories/FetchBacklog.js
@@ -0,0 +1,17 @@
+import { API_TOKEN, API_URL} from "../../constants/constants.js"
+
+export const fetchStatus = async () => {
+    const response = await fetch (
+           `${API_URL}/statuses?populate=*&filters[StatusType][$eq]=Backlog`,
+        {
+            headers: {
+                Authorization: `Bearer ${API_TOKEN}`,
+            },
+        }
+    );
+    if (!response.ok) {
+        throw new Error ("Fetching of statuses failed")
+    }
+    const data = await response.json();
+    return data
+};
