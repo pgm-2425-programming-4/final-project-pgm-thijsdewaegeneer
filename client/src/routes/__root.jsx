baf4a6a9cee42bf5429b92c5c50b7f4a12f2eab3
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-08-13 21:00:08 +0200

    Added routes

--- client/src/routes/__root.jsx
+++ client/src/routes/__root.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { createRootRoute, Outlet, Link } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { useQuery } from "@tanstack/react-query";
+import { fetchProjects } from "../queries/fetchProjects"
 
 
 export const Route = createRootRoute({
