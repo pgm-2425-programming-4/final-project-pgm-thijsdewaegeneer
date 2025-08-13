df99cb2d29829393028c02281e50b971ab4a9f32
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-08-13 18:06:32 +0200

    localhost constants added

---
+++ client/src/constants/constants.js
@@ -0,0 +1,7 @@
+export const API_URL = import.meta.env.PROD
+? ""
+: "http://localhost:1337/api";
+
+export const API_TOKEN = import.meta.env.PROD
+? ""
+: "5021e77fef9d38495f509733049c3a812c0b242f354ccf52c9641835910b575f2bd3dbef02c04da73588bfdc3d0d8277974334a646f16584404e2fbcfc1d87f3e7ab880fe196be92233d45f954f76b3a198873cb2ebbb163e2feba28a7c41d85f157f9b944d697d53f9bc799434963b544a764a07aa3a97955081777ad343eb1"
