673ea1ddb56d34a2eb2dbfb4a2bae1e36516ec33
pgm-thijsdewaegeneer <thijdewa@student.arteveldehs.be>
2025-08-13 23:04:54 +0200

    Changed name

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('body')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
